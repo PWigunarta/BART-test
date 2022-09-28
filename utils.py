4b31b0b1159710760f976829943a1fa02332253c
PWigunarta <pwigunarta@gmail.com>
2022-09-28 21:24:02 +0700

    update bart model

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ from filelock import FileLock
 
 from transformers import BartTokenizer, EvalPrediction, PreTrainedTokenizer, T5Tokenizer
 from transformers.file_utils import cached_property
-from transformers.modeling_bart.BartModel import shift_tokens_right
+from transformers.BartModel import shift_tokens_right
 
 try:
     import nltk
