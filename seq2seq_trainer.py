9ece84500c0a5a915a036b0f40c69264a99b0505
PWigunarta <pwigunarta@gmail.com>
2022-09-28 20:17:54 +0700

    reset import

--- seq2seq_trainer.py
+++ seq2seq_trainer.py
@@ -20,7 +20,6 @@ from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.dataset import Dataset
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data.sampler import RandomSampler, Sampler, SequentialSampler
-from transformers.utils import logging
 from transformers.data.data_collator import DataCollator, DataCollatorWithPadding, default_data_collator
 
 from transformers.configuration_fsmt import FSMTConfig
@@ -32,11 +31,38 @@ from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 
 from transformers.training_args import TrainingArguments
 
-from transformers.trainer_utils import *
+from transformers.trainer_utils import (
+    PREFIX_CHECKPOINT_DIR,
+    BestRun,
+    EvalPrediction,
+    EvaluationStrategy,
+    HPSearchBackend,
+    PredictionOutput,
+    TrainOutput,
+    default_compute_objective,
+    default_hp_space,
+    distributed_broadcast_scalars,
+    distributed_concat,
+    nested_concat,
+    nested_numpify,
+    nested_xla_mesh_reduce,
+    set_seed,
+)
 
-from transformers.trainer_callback import *
+from transformers.trainer_callback import (
+    TrainerState,
+)
 
-from transformers.integrations import *
+from transformers.integrations import (
+    default_hp_search_backend,
+    is_comet_available,
+    is_optuna_available,
+    is_ray_available,
+    is_tensorboard_available,
+    is_wandb_available,
+    run_hp_search_optuna,
+    run_hp_search_ray,
+)
 
 try:
     from transformers.utils import label_smoothed_nll_loss
